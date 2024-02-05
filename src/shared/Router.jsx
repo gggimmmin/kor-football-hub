00a0d69b693fe3350f962964d2a03caebe9cfa93
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-02-05 18:59:58 +0900

    feat: Home, Detail 페이지 라우터 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import Detail from "pages/Detail";
+import Home from "pages/Home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
